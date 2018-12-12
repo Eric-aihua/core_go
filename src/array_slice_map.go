d3e6870af236db3b0bd8e5c472dd3c91a181d58b
孙爱华 <sunaihua@360.net>
2018-12-12 11:55:19 +0800

    update sample

--- src/array_slice_map.go
+++ src/array_slice_map.go
@@ -7,13 +7,17 @@ import (
 
 //初始完大小后，数组大小不能发生变化
 func array_sample() {
-	a := []int{1, 2} //等同于a := [2]int{1, 2}
+	a := []int{1, 2, 3} //等同于a := [2]int{1, 2}
 	fmt.Println(a)
 	/**
 	// a在初始化的时候指定了大小为2,数据的大小不能自动伸缩，当对a[2]进行赋值时，会报index out of range错误
 	a[2] = 3
 	fmt.Println(a)
 	**/
+	//数组中的元素在内存中都是连续的地址
+	fmt.Printf("a[0]地址:%v\n", &a[0])
+	fmt.Printf("a[1]地址:%v\n", &a[1])
+	fmt.Printf("a[2]地址:%v", &a[2])
 }
 
 // 切片可以理解为增强版的数组，初始化大小后，还可以对数组进行扩容，删除等操作
@@ -37,7 +41,20 @@ func slice_sample() {
 	fmt.Println(slice_clone)
 }
 
+// map示例
+func map_sample() {
+	//key 为string类型，value为int类型
+	dict := make(map[string]int)
+	dict["m1"] = 1
+	dict["m2"] = 2
+	dict["m3"] = 3
+	fmt.Println(dict)
+	delete(dict, "m1")
+	fmt.Println(dict)
+}
+
 func main() {
-	//	array_sample()
-	slice_sample()
+	array_sample()
+	//	slice_sample()
+	//	map_sample()
 }
